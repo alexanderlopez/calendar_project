b3b4af34f321846f38e356f2deaa21d4c3a0c15b
Alexander Lopez <alexanderlopez00@gmail.com>
2020-07-17 14:16:31 -0400

    Add optional attendee functionality. Pass all tests

--- src/main/java/com/google/sps/FindMeetingQuery.java
+++ src/main/java/com/google/sps/FindMeetingQuery.java
@@ -18,47 +18,44 @@ import java.util.Collection;
 import java.util.ArrayList;
 import java.util.PriorityQueue;
 import java.util.Comparator;
+import java.util.Iterator;
 
 public final class FindMeetingQuery {
 
-    ArrayList<TimeRange> queryResult;
+    private ArrayList<TimeRange> queryResult;
+    private PriorityQueue<Event> guestEventQueue;
+    private PriorityQueue<Event> eventQueue;
 
     public Collection<TimeRange> query(Collection<Event> events,
             MeetingRequest request) {
         queryResult = new ArrayList<TimeRange>();
+        eventQueue =
+            new PriorityQueue<Event>((Event e1, Event e2) ->
+                TimeRange.ORDER_BY_START.compare(e1.getWhen(), e2.getWhen()));
+        guestEventQueue =
+            new PriorityQueue<Event>((Event e1, Event e2) ->
+                TimeRange.ORDER_BY_START.compare(e1.getWhen(), e2.getWhen()));
 
         TimeRange currentRange = TimeRange.WHOLE_DAY;
 
-        PriorityQueue<Event> eventQueue =
-                new PriorityQueue<Event>(new Comparator<Event>() {
-            @Override
-            public int compare(Event e1, Event e2) {
-                return TimeRange.ORDER_BY_START.compare(
-                        e1.getWhen(), e2.getWhen());
-            }
-        });
-
-        for (Event event : events) {
-            if (eventIntersectsGuestlist(event, request.getAttendees())) {
-                eventQueue.add(event);
-            }
-        }
+        checkEvents(events, request.getAttendees(),
+            request.getOptionalAttendees());
 
         while (!eventQueue.isEmpty()) {
-            TimeRange currentEvent = eventQueue.poll().getWhen();
+            TimeRange currentEventTimeRange = eventQueue.poll().getWhen();
 
-            if (!currentEvent.overlaps(currentRange)) {
+            if (!currentEventTimeRange.overlaps(currentRange)) {
                 continue;
             }
 
             TimeRange addRange = TimeRange.fromStartEnd(currentRange.start(),
-                                 currentEvent.start(), false);
+                                 currentEventTimeRange.start(), false);
 
             if (addRange.duration() >= request.getDuration()) {
                 queryResult.add(addRange);
             }
 
-            currentRange = TimeRange.fromStartEnd(currentEvent.end(),
+            currentRange = TimeRange.fromStartEnd(currentEventTimeRange.end(),
                            currentRange.end(), false);
             if (currentRange.duration() <= 0) {
                 break;
@@ -69,19 +66,151 @@ public final class FindMeetingQuery {
             queryResult.add(currentRange);
         }
 
-        return queryResult;
+        Collection<TimeRange> optionalCheck = considerOptional(
+            request.getDuration());
+
+        if (optionalCheck.isEmpty()) {
+            return queryResult;
+        }
+
+        return optionalCheck;
     }
 
-    private boolean eventIntersectsGuestlist(Event event,
-            Collection<String> guestList) {
-        Collection<String> eventGuestlist = event.getAttendees();
+    private Collection<TimeRange> considerOptional(long duration) {
+        ArrayList<TimeRange> optionalResult = new ArrayList<TimeRange>();
 
-        for (String name : guestList) {
-            if (eventGuestlist.contains(name)) {
-                return true;
+        if (queryResult.isEmpty()) {
+            return optionalResult;
+        }
+
+        if (guestEventQueue.isEmpty()) {
+            return optionalResult;
+        }
+
+        Iterator<TimeRange> availableRangeIterator = queryResult.iterator();
+        TimeRange workingTimeRange = availableRangeIterator.next();
+        TimeRange currentEventTimeRange = guestEventQueue.poll().getWhen();
+
+        eventIteration:
+        while (true) {
+
+            if (workingTimeRange.start() >= currentEventTimeRange.end()) {
+                if (guestEventQueue.isEmpty()) {
+                    break eventIteration;
+                }
+
+                currentEventTimeRange = guestEventQueue.poll().getWhen();
+            }
+
+            while (!currentEventTimeRange.overlaps(workingTimeRange)
+                    && workingTimeRange.start() < currentEventTimeRange.end()) {
+                if (!availableRangeIterator.hasNext()) {
+                    // Might be able too immediately return here.
+                    break eventIteration;
+                }
+
+                optionalResult.add(workingTimeRange);
+                workingTimeRange = availableRangeIterator.next();
+            }
+
+            if (currentEventTimeRange.overlaps(workingTimeRange)) {
+                TimeRange[] rangeCut = handleOverlap(
+                    currentEventTimeRange, workingTimeRange, duration);
+
+                if (rangeCut[0] != null) {
+                    optionalResult.add(rangeCut[0]);
+                }
+                if (rangeCut[1] != null) {
+                    workingTimeRange = rangeCut[1];
+                }
+                else {
+                    if (!availableRangeIterator.hasNext()) {
+                        workingTimeRange = null;
+                        break eventIteration;
+                    }
+
+                    workingTimeRange = availableRangeIterator.next();
+                }
             }
         }
 
-        return false;
+        while (workingTimeRange != null) {
+            optionalResult.add(workingTimeRange);
+
+            if (availableRangeIterator.hasNext()) {
+                workingTimeRange = availableRangeIterator.next();
+            }
+            else {
+                workingTimeRange = null;
+            }
+        }
+
+        return optionalResult;
+    }
+
+    private TimeRange[] handleOverlap(TimeRange eventRange,
+            TimeRange workingRange, long duration) {
+        TimeRange[] returnRange = new TimeRange[2];
+
+        if (eventRange.contains(workingRange)) {
+            return returnRange;
+        }
+        else if (workingRange.contains(eventRange)) {
+            TimeRange before = TimeRange.fromStartEnd(workingRange.start(),
+                eventRange.start(), false);
+            TimeRange after = TimeRange.fromStartEnd(eventRange.end(),
+                workingRange.end(), false);
+
+            if (before.duration() >= duration) {
+                returnRange[0] = before;
+            }
+            if (after.duration() >= duration) {
+                returnRange[1] = after;
+            }
+        }
+        else if (workingRange.start() < eventRange.start()) {
+            TimeRange trimRange = TimeRange.fromStartEnd(workingRange.start(),
+                eventRange.start(), false);
+
+            if (trimRange.duration() >= duration) {
+                returnRange[0] = trimRange;
+            }
+        }
+        else {
+            TimeRange trimRange = TimeRange.fromStartEnd(eventRange.end(),
+                workingRange.end(), false);
+
+            if (trimRange.duration() >= duration) {
+                returnRange[1] = trimRange;
+            }
+        }
+
+        return returnRange;
+    }
+
+    private void checkEvents(Collection<Event> events,
+            Collection<String> guestList, Collection<String> optionalList) {
+
+        for (Event event : events) {
+            Collection<String> eventGuestList = event.getAttendees();
+            boolean hasAttendee = false;
+            boolean hasOptionalAttendee = false;
+
+            for (String guest : eventGuestList) {
+                if (guestList.contains(guest)) {
+                    hasAttendee = true;
+                }
+                if (optionalList.contains(guest)) {
+                    hasOptionalAttendee = true;
+                }
+            }
+
+            if (hasAttendee) {
+                eventQueue.add(event);
+            }
+            else if (hasOptionalAttendee) {
+                guestEventQueue.add(event);
+            }
+        }
     }
 }
